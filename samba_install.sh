49258189bccc0791f782984e789f46046407edec
Shavyrin Dmitry <Dmitry.Shavyrin@paragon-software.com>
2015-05-18 09:42:54 +0300

    Adding INSTALL_PREFIX/C_SAMBA_PREFIX  envs

--- samba_install.sh
+++ samba_install.sh
@@ -7,11 +7,13 @@ CROSS_COMPILE=${C_TOOLCHAIN_UTILITES_HOST}-
 python_ROOT_DIR_EMB=${top_dir}/python_bin/
 NCURSES_CONFIG_EMB=${top_dir}/ncurses_bin/
 DESTDIR_ENV=${top_dir}/smb_bin
+INSTALL_PREFIX=${C_SAMBA_PREFIX}
 export QEMU_PATH_ENV
 export CROSS_COMPILE
 export python_ROOT_DIR_EMB
 export NCURSES_CONFIG_EMB
 export DESTDIR_ENV
+export INSTALL_PREFIX
 
 make configure_embedded
 make build_embedded
